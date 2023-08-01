9a52ff5d51d2d94b5f614795bdca52d461e65881
AbdurrahmanOzmen <abdurrahmanozmen250@gmail.com>
2023-08-01 16:22:34 +0300

    DbContext güncellendi

---
+++ Entities/Entities/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class AppContext:DbContext
+    {
+       // private IHttpContextAccessor _httpContextAccessor;
+        public IConfiguration Configuration { get; }
+    }
+}

42f80e8a2e60106909613ec7df195acb63545ad4
AbdurrahmanOzmen <abdurrahmanozmen250@gmail.com>
2023-08-01 16:00:54 +0300

    Entity katmanı güncellendi.

---
+++ Entities/Entities/Activity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+   public class Activity : AuditableBaseEntity
+    {
+
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+    
+
+
+    }
+}
---
+++ Entities/Entities/AuditableBaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class AuditableBaseEntity :BaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+        public int CreatedUserId { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedUserId { get; set; }
+    }
+}

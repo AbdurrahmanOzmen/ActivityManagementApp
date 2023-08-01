c4639389a6dc638e86ee907f58cefc18260cfd00
AbdurrahmanOzmen <abdurrahmanozmen250@gmail.com>
2023-08-01 16:03:58 +0300

    Katmanlar arası haberleşme sağlandı

--- Entities/Entities/User.cs
+++ Entities/Entities/User.cs
@@ -13,8 +13,7 @@ namespace Entities.Entities
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        
+        public string Password { get; set; }       
 
 
     }

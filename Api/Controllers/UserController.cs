e36c51bcd950fc420ea7e1b563c17b463ef4219c
Levent Gürsoy TONKAL <gursoy.tonkal@bil.omu.edu.tr>
2023-08-15 12:43:20 +0300

    using entity fixed.

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -5,9 +5,7 @@ using Entities.Enums;
 using ESPV2.OperationManagement.Api.Infrastructure.Attributes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
